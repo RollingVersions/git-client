09c757ca394d6fe135e2f9ddbc22b5a7a5b23a8e
Forbes Lindesay <forbes@lindesay.co.uk>
2022-02-20 12:08:51 +0000

    fix: another test case

--- packages/http/src/initialRequest.integration.ts
+++ packages/http/src/initialRequest.integration.ts
@@ -13,7 +13,7 @@ test('initialRequest', async () => {
   );
   expect(typeof capabilities.get('agent')).toBe('string');
   expect(capabilities.get('ls-refs')).toBe('unborn');
-  expect(capabilities.get('fetch')).toBe('shallow filter');
+  expect(capabilities.get('fetch')).toBe('shallow wait-for-done filter');
   expect(capabilities.get('server-option')).toBe(true);
   expect(capabilities.get('object-format')).toBe('sha1');
 });
