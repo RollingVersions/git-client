1c17c0dc77cff4cd292afaffaec375c415d188ae
Forbes Lindesay <forbes@lindesay.co.uk>
2022-03-04 16:53:04 +0000

    perf: mostly working but not quite

--- packages/packfile/src/PackfileParserStream.ts
+++ packages/packfile/src/PackfileParserStream.ts
@@ -6,6 +6,14 @@ import {createInflate, Inflate} from 'zlib';
 import applyDelta from './apply-delta';
 import {GitObjectTypeID} from './types';
 
+const START = Date.now();
+
+const expectedTypes = require('fs')
+  .readFileSync(`fetch-response-types.txt`, `utf8`)
+  .split(`\n`);
+
+const INT32_BYTES = 4;
+
 // The first four bytes in a packfile are the bytes 'PACK'
 
 // The version is stored as an unsigned 32 integer in network byte order.
@@ -36,18 +44,15 @@ export default class PackfileParserStream extends Transform {
   constructor({references = new Map(), offsets = new Map()}: Stores = {}) {
     // Global State
     let state: State = 'PACK_WORD';
+    const buffer = createBuffer();
     // const digest = createHash('sha1');
     let version = 3;
     let entriesCount = 0;
     let entryIndex = 0;
-    let offset = 0;
     let calls = 0;
 
     // parse step state
-    let chunk: Buffer;
-    let chunkOffset = 0;
     let currentCallback!: Callback;
-    let currentParseStepStartingOffset = 0;
     let currentParseStepStartingState: State = state;
 
     // Entry State
@@ -58,16 +63,37 @@ export default class PackfileParserStream extends Transform {
     let currentRefDeltaId = '';
     let currentOffsetDeltaId = 0;
     let inflateError: Error | null = null;
-    let inflateInputBuffer!: Buffer[];
+    let inflateBytesWritten = 0;
     let inflateOutputBuffer!: Buffer[];
     let inflate!: Inflate;
 
+    let currentInflateStreamCallback: Callback | undefined;
+    let inflateHappened = {resume: true, event: true};
+
+    let streamEnded = false;
+
+    const checkExpected = () => {
+      const expected = JSON.parse(expectedTypes[entryIndex]);
+      if (
+        expected.type !== currentEntryType ||
+        expected.entryOffset !== currentEntryOffset ||
+        expected.size !== currentEntrySize
+      ) {
+        console.log(`expected = `, expected);
+        console.log(`actual = `, {
+          type: currentEntryType,
+          entryOffset: currentEntryOffset,
+          size: currentEntrySize,
+        });
+      } else {
+        console.log(`✅`);
+      }
+    };
     const states = {
       PACK_WORD(cb: Callback) {
-        if (chunk.length - chunkOffset < 4) return cb();
+        if (buffer.getLength() < INT32_BYTES) return cb();
 
-        const packfileHeader = chunk.readUInt32BE(chunkOffset);
-        chunkOffset += 4;
+        const packfileHeader = buffer.consumeUInt32BE();
 
         if (packfileHeader !== 0x5041434b) {
           throw new Error(
@@ -79,10 +105,9 @@ export default class PackfileParserStream extends Transform {
         cb();
       },
       VERSION(cb: Callback) {
-        if (chunk.length - chunkOffset < 4) return cb();
+        if (buffer.getLength() < INT32_BYTES) return cb();
 
-        version = chunk.readUInt32BE(chunkOffset);
-        chunkOffset += 4;
+        version = buffer.consumeUInt32BE();
 
         if (version !== 2 && version !== 3) {
           throw new Error(
@@ -94,10 +119,9 @@ export default class PackfileParserStream extends Transform {
         cb();
       },
       ENTRIES_COUNT(cb: Callback) {
-        if (chunk.length - chunkOffset < 4) return cb();
+        if (buffer.getLength() < INT32_BYTES) return cb();
 
-        entriesCount = chunk.readUInt32BE(chunkOffset);
-        chunkOffset += 4;
+        entriesCount = buffer.consumeUInt32BE();
 
         if (entriesCount === 0) {
           state = 'CHECKSUM';
@@ -108,7 +132,7 @@ export default class PackfileParserStream extends Transform {
         cb();
       },
       HEADER(cb: Callback) {
-        if (chunk.length - chunkOffset < 1) return cb();
+        if (!buffer.getLength()) return cb();
 
         if (entriesCount <= entryIndex) {
           state = 'CHECKSUM';
@@ -117,43 +141,42 @@ export default class PackfileParserStream extends Transform {
 
         // Setup the inflate stream
         inflateError = null;
-        inflateInputBuffer = [];
+        inflateBytesWritten = 0;
         inflateOutputBuffer = [];
         inflate = initializeInflateStream();
 
-        currentEntryOffset = offset;
+        currentEntryOffset = buffer.getOffset();
 
-        const byte = chunk[chunkOffset];
+        const byte = buffer.consumeByte();
         currentEntryType = (byte >> 4) & 0x7;
         currentEntrySize = byte & 0xf;
         currentEntryLeftShift = 4;
-
-        chunkOffset++;
 
         if (byte & 0x80) {
           state = 'HEADER_EXTENSION';
         } else {
           state = getCurrentEntryBodyType(currentEntryType);
+          checkExpected();
         }
         cb();
       },
       HEADER_EXTENSION(cb: Callback) {
-        while (chunk.length > chunkOffset) {
-          const byte = chunk[chunkOffset++];
+        while (buffer.getLength()) {
+          const byte = buffer.consumeByte();
           currentEntrySize |= (byte & 0x7f) << currentEntryLeftShift;
           currentEntryLeftShift += 7;
           if (!(byte & 0x80)) {
             state = getCurrentEntryBodyType(currentEntryType);
-            return cb();
+            checkExpected();
+            break;
           }
         }
         cb();
       },
       OFS_DELTA(cb: Callback) {
-        if (chunk.length - chunkOffset < 1) return cb();
-        const byte = chunk[chunkOffset];
+        if (buffer.getLength()) return cb();
+        const byte = buffer.consumeByte();
         currentOffsetDeltaId = byte & 0x7f;
-        chunkOffset++;
         if (byte & 0x80) {
           state = 'OFS_DELTA_EXTENSION';
         } else {
@@ -162,8 +185,8 @@ export default class PackfileParserStream extends Transform {
         cb();
       },
       OFS_DELTA_EXTENSION(cb: Callback) {
-        while (chunk.length > chunkOffset) {
-          const byte = chunk[chunkOffset++];
+        while (buffer.getLength()) {
+          const byte = buffer.consumeByte();
           currentOffsetDeltaId =
             ((currentOffsetDeltaId + 1) << 7) | (byte & 0x7f);
           if (!(byte & 0x80)) {
@@ -174,17 +197,42 @@ export default class PackfileParserStream extends Transform {
         cb();
       },
       REF_DELTA(cb: Callback) {
-        if (chunk.length - chunkOffset < 20) return cb();
-        currentRefDeltaId = chunk.slice(0, 20).toString('hex');
-        chunkOffset += 20;
+        if (buffer.getLength() < 20) return cb();
+        currentRefDeltaId = buffer.readMax().slice(0, 20).toString('hex');
+        buffer.consume(20);
+        state = 'RAW_BODY';
+        cb();
       },
       RAW_BODY(cb: Callback) {
         if (inflateError) throw inflateError;
-        const c = chunk.slice(chunkOffset);
-        console.log(c.length, chunkOffset, chunk.length);
-        chunkOffset = chunk.length;
-        inflateInputBuffer.push(c);
-        inflate.write(c, cb);
+        const chunk = buffer.readMax();
+        inflateBytesWritten += chunk.length;
+        buffer.consume(chunk.length);
+
+        inflateHappened = {resume: false, event: false};
+        currentInflateStreamCallback = cb;
+        if (streamEnded) {
+          inflate.end(chunk);
+        } else {
+          inflate.write(chunk, onInflateWrite);
+        }
+        // const writeBufferLength = inflateBytesWritten - inflate.bytesWritten;
+
+        // console.log(`🚨 ${Date.now() - START} add callback`);
+        // currentInflateStreamCallback = (err) => {
+        //   console.log(`✅ ${Date.now() - START} event`);
+        //   cb(err);
+        // };
+        // console.log(`⭐ ${Date.now() - START} writing`);
+        // inflate.write(chunk, (err) => {
+        //   console.log(`⭐ ${Date.now() - START} cb`);
+        //   if (err) return cb(err);
+        //   // if (writeBufferLength > 256 * 1_024) {
+        //   // } else {
+        //   //   cb();
+        //   // }
+        // });
+        // console.log(`⭐ ${Date.now() - START} written`);
       },
       BODY_END(cb: Callback) {
         const outputBuffer = Buffer.concat(inflateOutputBuffer);
@@ -194,12 +242,10 @@ export default class PackfileParserStream extends Transform {
           );
         }
 
-        const inputBuffer = Buffer.concat(inflateInputBuffer);
+        console.log(`inflateBytesWritten=`, inflateBytesWritten);
         // TODO: digest?
         // digest.update(inputBuffer.slice(0, inflate.bytesWritten));
-        const remaining = inputBuffer.slice(inflate.bytesWritten);
-        chunkOffset = 0;
-        chunk = remaining;
+        buffer.unConsume(inflateBytesWritten - inflate.bytesWritten);
 
         const entryBodyType = getCurrentEntryBodyType(currentEntryType);
         const rawBody = Buffer.concat(inflateOutputBuffer);
@@ -235,10 +281,15 @@ export default class PackfileParserStream extends Transform {
           default:
             assertNever(entryBodyType);
         }
+
+        entryIndex++;
         state = 'HEADER';
         cb();
       },
       CHECKSUM(cb: Callback) {
+        if (buffer.getLength() < 20) return cb();
+        const checksum = buffer.readMax().slice(0, 20).toString('hex');
+        console.info(`Expected checksum ${checksum}`);
         // TODO: validate checksum
         cb();
       },
@@ -248,13 +299,13 @@ export default class PackfileParserStream extends Transform {
 
     super({
       readableObjectMode: true,
-      transform(rawChunk, _, callback) {
+      transform(chunk, _, callback) {
         calls++;
         if (calls > 1) {
           throw new Error(`concurrent calls encountered`);
         }
         currentCallback = callback;
-        chunk = chunk ? Buffer.concat([chunk, rawChunk]) : rawChunk;
+        buffer.push(chunk);
         parseStep();
       },
       flush(callback) {
@@ -262,8 +313,10 @@ export default class PackfileParserStream extends Transform {
         if (calls > 1) {
           throw new Error(`concurrent calls encountered`);
         }
-        // TODO: handle possibility that state is ok
-        callback(new Error(`Unexpected end of stream`));
+        currentCallback = callback;
+        parseStep();
+        // // TODO: handle possibility that state is ok
+        // callback(new Error(`Unexpected end of stream`));
       },
     });
 
@@ -273,30 +326,62 @@ export default class PackfileParserStream extends Transform {
       const stream = createInflate();
       stream
         .on(`data`, (chunk) => {
-          if (!calls) {
-            console.warn(`🚨 no active call`);
-          }
           inflateOutputBuffer.push(chunk);
+          console.log(`⭐ ${Date.now() - START} data`);
+          onInflateEvent();
         })
         .on('error', (err) => {
-          if (!calls) {
-            console.warn(`🚨 no active call`);
-          }
+          console.log(`⭐ ${Date.now() - START} error`);
           inflateError = err;
-          console.error(err.stack);
+          if (streamEnded && currentInflateStreamCallback) {
+            const cb = currentInflateStreamCallback;
+            currentInflateStreamCallback = undefined;
+            cb();
+          } else {
+            onInflateEvent(err);
+          }
         })
         .on('end', () => {
-          if (!calls) {
-            console.warn(`🚨 no active call`);
-          }
+          console.log(`⭐ ${Date.now() - START} end`);
           state = 'BODY_END';
+          if (streamEnded && currentInflateStreamCallback) {
+            const cb = currentInflateStreamCallback;
+            currentInflateStreamCallback = undefined;
+            cb();
+          } else {
+            onInflateEvent();
+          }
         });
       return stream;
+    }
+    function onInflateEvent(err?: Error | null | undefined) {
+      inflateHappened.event = true;
+      if (
+        !streamEnded &&
+        (inflateHappened.resume || err) &&
+        currentInflateStreamCallback
+      ) {
+        const cb = currentInflateStreamCallback;
+        currentInflateStreamCallback = undefined;
+        cb(err);
+      }
+    }
+    function onInflateWrite(err?: Error | null | undefined) {
+      console.log(`⭐ ${Date.now() - START} resume`);
+      inflateHappened.resume = true;
+      if (
+        !streamEnded &&
+        (inflateHappened.event || err) &&
+        currentInflateStreamCallback
+      ) {
+        const cb = currentInflateStreamCallback;
+        currentInflateStreamCallback = undefined;
+        cb(err);
+      }
     }
 
     function parseStep() {
       console.log(state);
-      currentParseStepStartingOffset = chunkOffset;
       currentParseStepStartingState = state;
       try {
         states[state](parseStepCallback);
@@ -306,27 +391,25 @@ export default class PackfileParserStream extends Transform {
     }
 
     function parseStepCallback(err?: Error | null | undefined) {
+      if (state === 'HEADER') {
+        buffer.trim();
+      }
       if (err) {
         calls--;
         currentCallback(err);
         return;
       }
 
-      if (
-        currentParseStepStartingOffset === chunkOffset &&
-        currentParseStepStartingState === state
-      ) {
-        // No data was consumed, we must be waiting for more data
+      if (currentParseStepStartingState === state) {
+        // If there were no state changes, we must be waiting for more data
         calls--;
         currentCallback();
         return;
       }
 
-      if (chunkOffset >= chunk.length) {
+      if (!buffer.getLength()) {
         // The entire chunk has been consumed
         // TODO: update the digest
-        chunkOffset = 0;
-        chunk = undefined as any;
         calls--;
         currentCallback();
         return;
@@ -366,4 +449,140 @@ function getCurrentEntryBodyType(typeId: number) {
 
 function encodeRaw(type: string, bytes: Uint8Array) {
   return concat(encode(`${type} ${bytes.length}\0`), bytes);
+}
+
+function createBuffer() {
+  let trimmedBytesCount = 0;
+  let bufferOffset = 0;
+  let bufferLength = 0;
+  let buffer: Buffer[] = [];
+
+  function push(chunk: Buffer) {
+    console.log(`🔼 push(${chunk.length})`);
+    buffer.push(chunk);
+    bufferLength += chunk.length;
+  }
+
+  function getLength() {
+    return bufferLength - bufferOffset;
+  }
+  function getOffset() {
+    return trimmedBytesCount + bufferOffset;
+  }
+
+  function readByte(index: number) {
+    let i = index + bufferOffset;
+    for (const chunk of buffer) {
+      if (i < chunk.length) {
+        return chunk[i];
+      } else {
+        i -= chunk.length;
+      }
+    }
+    throw new Error(`${index} out of range for buffer`);
+  }
+  function consumeByte() {
+    const result = readByte(0);
+    consume(1);
+    return result;
+  }
+
+  function readUInt32BE(index: number): number {
+    if (buffer.length > 1) {
+      console.warn(`🚨 concatenating buffers for readUInt32BE`);
+      buffer = [Buffer.concat(buffer)];
+    }
+    let i = index + bufferOffset;
+    if (i + INT32_BYTES > bufferLength) {
+      throw new Error(`${index} + INT32_BYTES out of range for buffer`);
+    }
+    for (const chunk of buffer) {
+      if (i < chunk.length) {
+        // if (i + INT32_BYTES <= chunk.length) {
+        return chunk.readUInt32BE(i);
+        // } else {
+        //   console.warn(`🚨 concatenating buffers`);
+        //   buffer = [Buffer.concat(buffer)];
+        //   return readUInt32BE(index);
+        // }
+      } else {
+        i -= chunk.length;
+      }
+    }
+    throw new Error(`${index} out of range for buffer`);
+  }
+  function consumeUInt32BE() {
+    const result = readUInt32BE(0);
+    consume(4);
+    return result;
+  }
+
+  function readMax(): Buffer {
+    const newBuffer = [];
+    let remainingOffset = bufferOffset;
+    for (const chunk of buffer) {
+      if (chunk.length <= remainingOffset) {
+        remainingOffset -= chunk.length;
+      } else if (remainingOffset) {
+        newBuffer.push(chunk.slice(remainingOffset));
+        remainingOffset = 0;
+      } else {
+        newBuffer.push(chunk);
+      }
+    }
+    if (newBuffer.length > 1) {
+      console.warn(`🚨 concatenating buffers for readMax`);
+    }
+    return Buffer.concat(newBuffer);
+  }
+
+  function consume(bytes: number) {
+    console.log(`🔽 consume(${bytes})`);
+    if (bufferOffset + bytes > bufferLength) {
+      throw new Error(
+        `There are not enough bytes in the buffer to consume ${bytes} bytes.`,
+      );
+    }
+    bufferOffset += bytes;
+  }
+
+  function unConsume(bytes: number) {
+    console.log(`🔼 unConsume(${bytes})`);
+    if (bytes > bufferOffset) {
+      throw new Error(
+        `There are not enough bytes in the offset to un-consume ${bytes} bytes. You can only un-consume ${bufferOffset} bytes.`,
+      );
+    }
+    bufferOffset -= bytes;
+  }
+
+  function trim() {
+    trimmedBytesCount += bufferOffset;
+    const newBuffer = [];
+    for (const chunk of buffer) {
+      if (chunk.length <= bufferOffset) {
+        bufferOffset -= chunk.length;
+      } else if (bufferOffset) {
+        newBuffer.push(chunk.slice(bufferOffset));
+        bufferOffset = 0;
+      } else {
+        newBuffer.push(chunk);
+      }
+    }
+    buffer = newBuffer;
+  }
+
+  return {
+    push,
+    getLength,
+    getOffset,
+    readByte,
+    consumeByte,
+    readUInt32BE,
+    consumeUInt32BE,
+    readMax,
+    consume,
+    unConsume,
+    trim,
+  };
 }
