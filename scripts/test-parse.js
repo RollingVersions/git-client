a19834ae348ceaf6928266b9b064c03e72cfe1da
Forbes Lindesay <forbes@lindesay.co.uk>
2022-03-04 19:42:41 +0000

    test: make sure back-pressure works correctly

--- scripts/test-parse.js
+++ scripts/test-parse.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const {parsePackfile} = require('../packages/packfile');
+const {PackfileParserStreamV2} = require('../packages/packfile');
 const gitObj = require('../packages/objects');
+const {Transform} = require('stream');
 
 const START = Date.now();
 async function parse() {
@@ -10,23 +11,42 @@ async function parse() {
   let j = 0;
   let line = 0;
   console.info(`each . is 1000 entries, each line is 10,000 entries`);
-  await parsePackfile(fs.readFileSync(filename), (entry) => {
-    count++;
-    i++;
-    if (i === 1000) {
-      i = 0;
-      process.stdout.write(`.`);
-      j++;
-      if (j === 10) {
-        j = 0;
-        process.stdout.write(
-          ` ${(line++).toString().padStart(6, ` `)} ${(Date.now() - START)
-            .toString()
-            .padStart(6, ` `)}ms\n`,
-        );
-      }
-    }
-    gitObj.decodeObject(entry.body);
+  const entries = new PackfileParserStreamV2(fs.readFileSync(filename));
+  await new Promise((resolve, reject) => {
+    entries
+      .on('error', reject)
+      .pipe(
+        new Transform({
+          writableObjectMode: true,
+          highWaterMark: 5,
+          transform(entry, _, cb) {
+            count++;
+            i++;
+            if (i === 1000) {
+              i = 0;
+              process.stdout.write(`.`);
+              j++;
+              if (j === 10) {
+                j = 0;
+                process.stdout.write(
+                  ` ${(line++).toString().padStart(6, ` `)} ${(
+                    Date.now() - START
+                  )
+                    .toString()
+                    .padStart(6, ` `)}ms\n`,
+                );
+              }
+            }
+
+            gitObj.decodeObject(entry.body);
+            cb(null, Buffer.concat([Buffer.from(entry.type), entry.body]));
+          },
+        }),
+      )
+      .on('error', reject)
+      .pipe(fs.createWriteStream(`temp/raw-data-stream.dat`))
+      .on('error', reject)
+      .on('close', resolve);
   });
 
   process.stdout.write(`\n\n`);
